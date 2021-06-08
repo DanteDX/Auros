3d22e1b1ccf397b64df1b3ff17a76c07d6cd50a3
YamatoDX <piyal@twurs.com>
2021-06-08 18:38:56 +0600

    FirebaseProvider created

---
+++ client/components/wrappers/FirebaseProvider.tsx
@@ -0,0 +1,22 @@
+import React, {useState,createContext} from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/auth";
+import "firebase/storage";
+import {firebaseConfig} from "@/config/firebaseConfig";
+
+if(firebase.apps.length === 0){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const FirebaseContext = createContext({});
+
+export default function FirebaseProvider({children}){
+    const [state,setState] = useState({});
+    return(
+        <FirebaseContext.Provider value={state}>
+            {children}
+        </FirebaseContext.Provider>
+    )
+}
