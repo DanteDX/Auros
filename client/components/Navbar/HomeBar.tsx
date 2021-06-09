4e0b71bf5918b86e6cafe2556f0dce3de8af0eca
YamatoDX <piyal@twurs.com>
2021-06-09 15:19:49 +0600

    Login, logout, signup li added

--- client/components/Navbar/HomeBar.tsx
+++ client/components/Navbar/HomeBar.tsx
@@ -2,17 +2,41 @@ import React from "react";
 import styles from "@/styles/HomeBar.module.css";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {fuego} from "@nandorojo/swr-firestore";
+import Link from "next/link";
 
 export default function HomeBar(){
-    const [user] = useAuthState(fuego.auth());
+    const [user,loading] = useAuthState(fuego.auth());
     return(
         <>
             <nav className={styles.wrapper}>
                 <ul className={styles.list}>
-                    <li>Home</li>
-                    <li>Profile</li>
-                    <li>Settings</li>
-                    <li>{user ? "Logged In" : "Logged Out"}</li>
+                    <ul>
+                        <li>Home</li>
+                        <li>Profile</li>
+                        <li>Settings</li>
+                    </ul>
+                    <ul>
+                        {!user ? (
+                            <>
+                                <li>
+                                    <Link href="/signup">
+                                        <a>Sign Up</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/login">
+                                        <a>Log In</a>
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li onClick={e => fuego.auth().signOut()}>
+                                    Log Out
+                                </li>
+                            </>
+                        )}
+                    </ul>
                 </ul>
             </nav>
         </>
